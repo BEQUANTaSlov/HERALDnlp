5e06799226fbb93c39b9eba11b7da05a44761c57
Slov <83311951+BEQUANTaSlov@users.noreply.github.com>
2021-09-13 21:28:27 -0400

    Add files via upload

--- BEQone4Altp2.py
+++ BEQone4Altp2.py
@@ -9,7 +9,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-with open('/Users/aidanslovinski/Desktop/HERALD/Tweet_Output.txt','r') as file:
+with open('/Users/aidanslovinski/Downloads/BEQUANT/Tweet_Output.txt','r') as file:
    status = file.read()
 
 #with open('Tweet_Output.txt','r') as file:
